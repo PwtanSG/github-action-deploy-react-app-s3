cedad00f83b6c247914df77752fa0912cbf4cd0b
pwtansg <pwtan.edu@gmail.com>
2025-06-20 17:32:15 +0800

    Update workflow to use repo variable

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          create-react-app is dead!
+          create-react-app is already dead!
         </a>
       </header>
     </div>
